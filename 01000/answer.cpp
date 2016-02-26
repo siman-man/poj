82f5614bf93070a1c7936d4d6c04fa0934835bb1
siman-man <k128585@ie.u-ryukyu.ac.jp>
2016-02-27 01:08:20 +0900

    first commit

---
+++ 01000/answer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <limits.h>
+#include <time.h>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <set>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+  int a, b;
+  cin >> a >> b;
+
+  cout << a + b << endl;
+  return 0;
+}
